01710fb6c1c48b57cf938ee0d2a4287dc398fcf6
Luis7 <luishgarcia_97@hotmail.com>
2024-07-20 10:33:55 -0500

    Actualice el proyecto con mejoras de conexión a la api

---
+++ javascript/conexionAPI.js
@@ -0,0 +1,40 @@
+async function listarVideos(){
+    const conexion = await fetch('https://my-json-server.typicode.com/Luis7Garcia/Api-AluraPlay/videos');
+
+    const conexionConvertida = await conexion.json();
+
+   // console.log(conexionConvertida);
+   return conexionConvertida
+}
+
+async function enviarVideo(titulo, descripcion, url, imagem){
+    const conexion = await fetch('https://my-json-server.typicode.com/Luis7Garcia/Api-AluraPlay/videos', {
+        method:"POST",
+        headers:{"content-type":"application/json"},
+        body:JSON.stringify({
+            titulo:titulo,
+            descripcion:`${descripcion} mil visualizaciones`,
+            url:url,
+            imagem:imagem
+        })
+    })
+    const conexionConvertida = conexion.json();
+
+    if(!conexion.ok){
+        throw new Error("Ha ocurrido un error al enviar el video");
+    }
+
+    return conexionConvertida;
+}
+
+async function buscarVideos(palabraClave){
+    const conexion = await fetch(`https://my-json-server.typicode.com/Luis7Garcia/Api-AluraPlay/videos?q=${palabraClave}`);
+    const conexionConvertida = conexion.json();
+    return conexionConvertida
+}
+
+export const conexionAPI ={
+    listarVideos, enviarVideo, buscarVideos
+}
+
+//listarVideos();
